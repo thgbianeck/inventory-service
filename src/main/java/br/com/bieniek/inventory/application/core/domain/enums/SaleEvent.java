a2c315014bf94ca2ef41d59f39c0d599db92be34
Bianeck <thiagobianeck@gmail.com>
2023-12-20 17:48:17 -0300

    feat: Criando as classes de domínio do débito do estoque

---
+++ src/main/java/br/com/bieniek/inventory/application/core/domain/enums/SaleEvent.java
@@ -0,0 +1,9 @@
+package br.com.bieniek.inventory.application.core.domain.enums;
+
+public enum SaleEvent {
+    CREATED_SALE,
+    UPDATED_INVENTORY,
+    VALIDATED_PAYMENT,
+    ROLLBACK_INVENTORY,
+    FAILED_PAYMENT;
+}
